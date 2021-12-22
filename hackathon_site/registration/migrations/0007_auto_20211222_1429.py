e70295dede218c6658d0b4882c76f5f041be7049
YuYing Liang <yuying.liang00@gmail.com>
2021-12-22 14:36:19 -0500

    formatted with black

--- hackathon_site/registration/migrations/0007_auto_20211222_1429.py
+++ hackathon_site/registration/migrations/0007_auto_20211222_1429.py
@@ -6,18 +6,22 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registration', '0006_auto_20211222_1315'),
+        ("registration", "0006_auto_20211222_1315"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='application',
-            name='address1',
-            field=models.CharField(blank=True, help_text='e.g. 35 St George St', max_length=255, null=True),
+            model_name="application",
+            name="address1",
+            field=models.CharField(
+                blank=True, help_text="e.g. 35 St George St", max_length=255, null=True
+            ),
         ),
         migrations.AlterField(
-            model_name='application',
-            name='address2',
-            field=models.CharField(blank=True, help_text='e.g. Apt. No. 13', max_length=255, null=True),
+            model_name="application",
+            name="address2",
+            field=models.CharField(
+                blank=True, help_text="e.g. Apt. No. 13", max_length=255, null=True
+            ),
         ),
     ]
